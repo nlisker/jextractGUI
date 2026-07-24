d3e77d0dbe9741acfcdd1d5d0deb12523e1ac40c
nlisker <37422899+nlisker@users.noreply.github.com>
2026-07-24 06:35:05 +0300

    Partial test for SymbolsViewer

---
+++ src/test/java/org/nlisker/jextractGUI/SymbolsViewerTest.java
@@ -0,0 +1,102 @@
+package org.nlisker.jextractGUI;
+
+import static com.google.common.truth.Truth.*;
+
+import java.nio.file.Path;
+
+import javafx.scene.control.CheckBoxTreeItem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.nlisker.jextractGUI.model.Displayable;
+import org.nlisker.jextractGUI.model.Displayable.Header;
+import org.nlisker.jextractGUI.model.Displayable.MainHeader;
+
+@DisplayNameGeneration(ReplaceCamelCase.class)
+class SymbolsViewerTest {
+
+	@Nested
+	class WhenHeaderIsSelfContained {
+
+		private static final String HEADER_REL_PATH = "full.h";
+
+		// level 1
+		private CheckBoxTreeItem<Displayable> mainHeaderItem;
+
+		// level 2 self-item
+		private CheckBoxTreeItem<Displayable> headerItem;
+
+		@BeforeAll
+		void buildTree() throws Exception {
+			var mainHeader = new MainHeader(Path.of(HEADER_REL_PATH));
+			var header = new Header(Path.of(HEADER_REL_PATH));
+
+			mainHeaderItem = new CheckBoxTreeItem<>(mainHeader);
+			headerItem = new CheckBoxTreeItem<>(header);
+
+			mainHeaderItem.getChildren().add(headerItem);
+
+			SymbolsViewer.selectSelfHeader(mainHeaderItem);
+		}
+
+		@Test
+		void selectMainHeaderAndSelfHeader() {
+			assertThat(mainHeaderItem.isSelected()).isTrue();
+			assertThat(headerItem.isSelected()).isTrue();
+		}
+
+		@Test
+		void expandMainHeaderAndSelfHeader() {
+			assertThat(mainHeaderItem.isExpanded()).isTrue();
+			assertThat(headerItem.isExpanded()).isTrue();
+		}
+	}
+
+	@Nested
+	class WhenHeaderHasIncludes {
+
+		private static final String INCLUDING = "including.h";
+		private static final String INCLUDED = "included.h";
+
+		// level 1
+		private CheckBoxTreeItem<Displayable> mainHeaderItem;
+
+		// level 2 self-item
+		private CheckBoxTreeItem<Displayable> includingHeaderItem;
+
+		// level 2 include
+		private CheckBoxTreeItem<Displayable> includedHeaderItem;
+
+		@BeforeAll
+		void buildTree() throws Exception {
+			var mainHeader = new MainHeader(Path.of(INCLUDING));
+			var includingHeader = new Header(Path.of(INCLUDING));
+			var includedHeader = new Header(Path.of(INCLUDED));
+
+			mainHeaderItem = new CheckBoxTreeItem<>(mainHeader);
+			includingHeaderItem = new CheckBoxTreeItem<>(includingHeader);
+			includedHeaderItem = new CheckBoxTreeItem<>(includedHeader);
+
+			mainHeaderItem.getChildren().add(includingHeaderItem);
+			mainHeaderItem.getChildren().add(includedHeaderItem);
+
+			SymbolsViewer.selectSelfHeader(mainHeaderItem);
+		}
+
+		@Test
+		void selectOnlyTheSelfHeader() {
+			assertThat(mainHeaderItem.isIndeterminate()).isTrue();
+			assertThat(includingHeaderItem.isSelected()).isTrue();
+			assertThat(includedHeaderItem.isSelected()).isFalse();
+		}
+
+		@Test
+		void expandOnlyTheSelfHeader() {
+			assertThat(mainHeaderItem.isExpanded()).isTrue();
+			assertThat(includingHeaderItem.isExpanded()).isTrue();
+			assertThat(includedHeaderItem.isExpanded()).isFalse();
+		}
+	}
+}
